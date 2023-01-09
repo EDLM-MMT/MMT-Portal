836e16779ef5e594ec423991fa8fb043dc66a6c6
schittlur <schittlur@deloitte.com>
2023-01-09 17:37:50 -0500

    Profile Page

---
+++ src/pages/profile.js
@@ -0,0 +1,48 @@
+import DefaultLayout from '@/components/layouts/DefaultLayout';
+import useStore from '@/store/store';
+import profileImage from '@/../public/profile-picture.png';
+import Image from 'next/image';
+
+export default function Profile() {
+    const userData = useStore((state) => state.userData);
+
+    console.log(userData);
+    const handleUpdate = (e) => {
+        
+    };
+
+    return (
+        <DefaultLayout >
+        <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
+            <h1 className='pb-4 border-b mb-8 text-3xl font-semibold'>Profile</h1>
+        
+        <Image src={profileImage} alt='DOD' width={200} height={200} />
+        <button type="submit" class="ml-8 mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Upload Image</button>
+
+
+        <form className='mt-4'>
+            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                    <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={userData?.learner.personnel.person.firstName} required />
+                </div>
+                <div>
+                    <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+                    <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={userData?.learner.personnel.person.lastName} required />
+                </div>
+                <div>
+                    <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="text" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={userData?.learner.personnel.person.email} required />
+                </div>  
+                <div>
+                    <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Position/Role</label>
+                    <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={userData?.role} required />
+                </div>
+            </div>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+        </form>
+
+        </div>
+        </DefaultLayout>
+    );
+}
