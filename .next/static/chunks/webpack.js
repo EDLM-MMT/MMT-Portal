f247d6129c6b16ad9d951b5a32b90362acab4be1
schittlur <schittlur@deloitte.com>
2023-01-09 10:42:50 -0500

    tailwind colors updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cacb61ba1f11a897"; }
+/******/ 		__webpack_require__.h = function() { return "604898a939033f48"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
