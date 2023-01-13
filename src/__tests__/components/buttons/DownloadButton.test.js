1a6f6a6533b3c758305e267aa8308300367983f2
schittlur <schittlur@deloitte.com>
2023-01-12 19:29:58 -0500

    Alert Component

--- src/__tests__/components/buttons/DownloadButton.test.js
+++ src/__tests__/components/buttons/DownloadButton.test.js
@@ -1,7 +1,7 @@
 import { act, fireEvent, render } from "@testing-library/react";
 import DownloadButton from "@/components/buttons/DownloadButton";
 
-describe("Button Component", () => {
+describe("Download Button Component", () => {
   it("should render the component", () => {
     const { getByText } = render(<DownloadButton handleDownloadClick={()=>{}}/>);
     expect(getByText(/Download/i)).toBeInTheDocument();
