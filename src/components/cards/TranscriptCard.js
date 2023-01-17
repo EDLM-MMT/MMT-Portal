e0466d087c7d550451dda45eeb8f33e88fa6632e
schittlur <schittlur@deloitte.com>
2023-01-17 17:04:15 -0500

    Transcript card pdf viewer update

--- src/components/cards/TranscriptCard.js
+++ src/components/cards/TranscriptCard.js
@@ -1,38 +1,13 @@
 import { useRouter } from "next/router"
 import DownloadButton from "@/components/buttons/DownloadButton";
 import Button from "../buttons/Button";
-import { Worker } from '@react-pdf-viewer/core';
-// Import the main component
-import { Viewer } from '@react-pdf-viewer/core';
-// Import the styles
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { useState } from "react";
 
 export default function TranscriptCard({ title, routePath, type}){
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-    const [file, setFile] = useState(null);
     const router = useRouter();
     const handleClick = () => {
         router.push(`/${routePath}`);
     }
 
-    const allowedFiles = ['application/pdf'];
-    const onFileChange= (event) => {
-        let selectedFile = event.target.files[0];
-        if(selectedFile){
-            if(selectedFile&&allowedFiles.includes(selectedFile.type)){
-              let reader = new FileReader();
-              reader.readAsDataURL(selectedFile);
-              reader.onloadend=(event)=>{
-               // setPdfError('');
-                setFile(event.target.result);
-              }
-            }
-        }
-    }
-    
     return(
         <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
             <h1 className='pb-4 border-b mb-2 text-3xl font-semibold'>
@@ -46,20 +21,11 @@ export default function TranscriptCard({ title, routePath, type}){
                 className='text-dod-500 hover:underline underline hover:text-blue-500 cursor-pointer transition-all duration-150 ease-in-out'>                    
                 Transcript </button> -{`>`} {type} Transcript 
             </div>
-            <div>
-                <label htmlFor="file">Load from file:</label>{' '}
-                <input onChange={onFileChange} type="file" />
-            </div>
 
             <div className="m-4 bg-gray-200 h-screen text-center">
-                {file &&(
-                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
-                    { <Viewer fileUrl={file} /*plugins={[defaultLayoutPluginInstance]}*/ /> }
-                    </Worker> 
-                )} 
-                {!file &&(
-                    <>No PDF file is selected yet</>
-                )}     
+                <object data={"https://www.africau.edu/images/default/sample.pdf"} type="application/pdf" width="100%" height="100%">
+                    test
+                </object>
             </div>
 
             <div className='flex flex-row align-bottom justify-between mt-5'>
