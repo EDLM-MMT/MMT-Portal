ce6a435ebf892907d2fc255861420bf53d20258a
schittlur <schittlur@deloitte.com>
2023-01-24 15:32:45 -0500

    center page fields

--- src/pages/resetPassword.js
+++ src/pages/resetPassword.js
@@ -45,8 +45,8 @@ export default function ResetPassword() {
             </div> 
         </h1>
 
-        <form className='mt-4' onSubmit={handleSubmit} onChange={handleUpdate}>
-            <div class="grid gap-6 mb-6 w-1/3">
+        <form className='flex flex-col mt-4 items-center' onSubmit={handleSubmit} onChange={handleUpdate}>
+            <div class="grid gap-6 mb-6 w-1/3 ">
                 <div>
                     <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> New Password</label>
                     <input type="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
@@ -57,7 +57,7 @@ export default function ResetPassword() {
                 </div>
             </div>
 
-            <div className='flex flex-row items-center justify-between w-1/3'>
+            <div className='flex flex-row items-center justify-between w-1/3 mt-2'>
                 <Button btnText={"Cancel"} link={"/profile"} className={"ml-0"}></Button>
                 <button onClick={handleSubmit} type="submit" class="text-white bg-blue-700 hover:bg-blue-800 h-10 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update Password</button>
             </div>
