e45f288f1cc3324966076e23440737c6b24607ee
schittlur <schittlur@deloitte.com>
2023-01-24 17:11:47 -0500

    Transcript card

--- src/components/cards/TranscriptCard.js
+++ src/components/cards/TranscriptCard.js
@@ -23,8 +23,9 @@ export default function TranscriptCard({ title, routePath, type}){
             </div>
 
             <div className="m-4 bg-gray-200 h-screen text-center">
-                <object data={"https://www.africau.edu/images/default/sample.pdf"} type="application/pdf" width="100%" height="100%">
-                    test
+                <object data={"https://jst-transcript.s3.us-east-2.amazonaws.com/Sample%20E_ACADEMIC_TRANSCRIPT.pdf?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEI3%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJGMEQCIGR5kNYIHeceqZe%2FHfOUR%2BJBTcyBC%2Fmt3qBmg6f6LA0YAiBtLUqdpFWsPUtK04YRgv1DIvnUyoY6PG3Q2v8Y%2FZ67yiq4Awj2%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAIaDDk1NTAzODU4MDE2NyIMETgJBBoZ5m0v5jdEKowDe6%2BTx2D8SEm23WtjXpjhWSHCPCtJQnhEoPdrfUMJYfuaGUGHTshVGtk4AIyghCj78hA6AXiTAYLCedQS0LVFacdsy22Uz5aeudnM03SgnpoUCiL46lVKoTrSsOlTZ8zI%2Fky0BP%2BOoD2XSwBJ5djWMZjG0SjSJ7Q61zae4fh66GkwDR0b72Q0oJt0Y0%2FNrd2y6XQGXRf7i4ueMovXqeeFqpV0%2FbXk9FXoTUcWclaaSlE84069yY9zl43dNoNcyaqIN%2FkMyoPdBpfM46t9OcydZtDUHAZFDy6lOU7S0FeHDhx8yroXRRJFcGnNu%2FzAjvVHA%2B69Rr2hQKupd9Bw30v3RpLaJTgUk7a7pjeYohsMuDB7dxwaINlizo4%2FgyUpeDa6MaF6H3Gll5IfdWR205HrEd0brmYPKkSCa7sXWZMdzE4SsbudQRI20C5OB8IUbSLDfMClP%2FWe1lDwCwQSGXqcoqL6lys2bP0B3o%2Fuj3Tvo1TZrciMQDSb9gyDjSe17og1GTwCY858yDfJsuymMJuEwZ4GOo0CNvNeebKuFt44tS0ncDY8b2wBr6aJS2c%2BV%2FIv8vRbbEmqrjjibfF6qHW5j5q054RPdioSryZnRnt6iFamo2TlKrIZPqIlAYTfLYJSDkiaZpGNDzZIXkSowra4PKocS0g1AjdOurtOphT%2FvYKRPD46rX9Y2tb3E5ktO%2BnXHn%2BAtDW89CEvC6DYYPM8nojD43JLJcENXXFKh%2FjgogxD5ouvgvEJ1FitA8S7%2BoNOG904uXTI8v%2FN7dkQzBw%2FYzaZrETHxPd8SeBvZp%2FLg54zoKOneQn4ubirFlMZ1%2BLR2QsXS%2FoELjM6gh6LD%2B1E%2FaPzDWPcNfc%2FU5RTeI3D8drtoLZzV1Y%2FOoeby2q0q40CnLI%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230124T212224Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIA54XF4HXDWJO2CYO6%2F20230124%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Signature=4601c7ef64abf20a37c21a3219ca6a7e6c23655c9a039884cd72744e26d838d7"}
+                    type="application/pdf" width="100%" height="100%">
+                    PDF Viewer
                 </object>
             </div>
 
