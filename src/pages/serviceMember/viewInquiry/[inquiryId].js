930603806a4b8f0c7e31d524bca23193dfc283ee
LFRMadrid <73301821+LFRMadrid@users.noreply.github.com>
2023-01-24 15:47:15 -0500

    Small changes to View Inquiry page
    changes provided based on feedback from team as well as refactoring the JSON file to be more readable and also add missing elements that would also be displayed in View Inquiry page such as inquiryComments section

--- src/pages/serviceMember/viewInquiry/[inquiryId].js
+++ src/pages/serviceMember/viewInquiry/[inquiryId].js
@@ -3,6 +3,7 @@ import ViewCard from '@/components/cards/ViewCard';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRouter } from "next/router"
+import useStore from '@/store/store';
 
 export function getServerSideProps(context) {
     const { inquiryId } = context.query;
@@ -15,6 +16,7 @@ export function getServerSideProps(context) {
 
 export default function InquiryView({inquiryId}) {
 
+  const userData = useStore((state) => state.userData);
   const currDate = new Date().toLocaleDateString();
   const currTime = new Date().toLocaleTimeString();
   const timestamp = `${currDate}  ${currTime}`;
@@ -44,14 +46,15 @@ export default function InquiryView({inquiryId}) {
 
     const [inquiry, setInquiry] = useState([]);
     const router = useRouter();
-    const [comments,setComments] = useState(mockData);
+    const [comments,setComments] = useState([]);
 
     useEffect(() => {
         axios
           .get(`/api/inquiry/${inquiryId}`)
           .then((res) => {
-            console.log("Result inside viewInquiry", res.data);
+            console.log("Result inside viewInquiry", res.data.inquiryComments);
             setInquiry(res.data);
+            setComments(res.data.inquiryComments);
           })
           .catch((err) => {
             console.log(err);
@@ -62,21 +65,19 @@ export default function InquiryView({inquiryId}) {
         router.push("/serviceMember/inquiries");
     }
 
-    const onSomeInputChange = (text) => {
-      const note = {
-        note: text,
-        created_at: Date.now()
-      };
-      setComments((prev) => ({
-        ...prev,
-        comment: text,
-    }));
+    const handlePost = (event) => {
+      event.preventDefault()
+      //console.log(event.target[0].value)
+      const newComment = {
+        author: `${userData?.learner.personnel.person.firstName} ${userData?.learner.personnel.person.lastName}`,
+        title:"",
+        comment: event.target.comment.value,
+        timestamp: timestamp,
+      }
+      setComments(comments =>[newComment, ...comments]);
+      event.target[0].value = "";
     }
-
-    console.log("Notes: ", comments);
-    const handlePost = () => {
-        router.push(`/serviceMember/transcripts`);
-    }
+    console.log(comments);
     
 
     //console.log("Whats inside data:", inquiry.id)
@@ -97,14 +98,16 @@ export default function InquiryView({inquiryId}) {
                     <ViewCard key={inquiry.id} title={inquiry.title} description={inquiry.description}/>                   
             </div>
             <div className='bg-white w-full border h-50 mt-4 rounded-md border-gray-200 p-4 pb-0 shadow'>
-                Submitted on {currDate} {currTime}
+                Submitted on {inquiry.timestampCreated}
             </div>
             <div className='bg-white w-full border h-50 mt-4 mb-4 rounded-md border-gray-200 p-4 pb-2 shadow'>
-              <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add a comment:</label>
-              <input type="text-area" id="description" onChange={onSomeInputChange} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-5/6 p-2.5 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={"Please provide comments if necessary."} required />
-              <button onClick={handlePost} className="flex inset-y-0 right-0 justify-center items-center tect-sm gap-2 dod-500 rounded-md hover:shadow-md text-white bg-dod-500/80 hover:bg-blue-400 hover:text-white px-2 p-1.5 transform transition-all duration-150 ease-in-out border-dod-500 border-2 focus:ring-2 ring-dod-500 outline-none">Post</button>
+              <form onSubmit={handlePost}>
+                <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add a comment:</label>
+                <input type="text-area" id="description" name="comment" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-5/6 p-2.5 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={"Please provide comments if necessary."} required />
+                <button className="flex justify-end items-center tect-sm gap-2 dod-500 rounded-md hover:shadow-md text-white bg-dod-500/80 hover:bg-blue-400 hover:text-white px-2 p-1.5 transform transition-all duration-150 ease-in-out border-dod-500 border-2 focus:ring-2 ring-dod-500 outline-none">Post</button>
+              </form>
             </div>
-            <div className="bold">
+            <div className="font-medium">
             Inquiry Timeline
             </div>
             {comments?.map((data, index) => {
