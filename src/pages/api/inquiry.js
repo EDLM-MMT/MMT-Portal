417f441bb4498a8e7590eb1d8013da5d3706012c
schittlur <schittlur@deloitte.com>
2023-01-25 16:20:19 -0500

    remove console logs

--- src/pages/api/inquiry.js
+++ src/pages/api/inquiry.js
@@ -1,6 +1,5 @@
 import inquiries from "@/data/service_member/inquiries.json"
 
 export default function handler(req, res) {
-    console.log("here", inquiries);
     return res.status(200).json(inquiries);
 }
