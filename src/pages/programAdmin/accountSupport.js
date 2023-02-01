0ee3af8ea60f14ff9868dcf785dd5504347ca9f7
schittlur <schittlur@deloitte.com>
2023-02-01 12:11:02 -0500

    account support table

--- src/pages/programAdmin/accountSupport.js
+++ src/pages/programAdmin/accountSupport.js
@@ -41,17 +41,17 @@ export default function AccountSupport() {
         <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
             <h1 className='pb-4 border-b mb-8 text-3xl font-semibold'>Account Support</h1>
             <div>
-                <input type="text" className=" w-1/2 mb-4 pl-4 bg-gray-50 border border-gray-300 text-gray-900 text-mid rounded-xl" placeholder="Search here" onChange={handleChange} value={searchInput} />
+                <input type="text" className=" w-1/2 mb-4 pl-4 bg-gray-50 border border-gray-300 text-gray-900 text-mid rounded-xl p-2" placeholder="Search here" onChange={handleChange} value={searchInput} />
             </div>
-            <table className='w-full border-separate ' style={{ borderSpacing: 0 }}>
+            <table className='w-full border-separate border' style={{ borderSpacing: 0 }}>
                 <thead className='bg-gray-50 '>
                     <tr>
                             <th scope='col'
                                 className='text-lg sticky top-0 z-10 hidden border-b
-                                    border-gray-300 bg-gray-50 bg-opacity-75 pl-2 py-2
-                                    text-left font-semibold text-gray-900 backdrop-blur
-                                    backdrop-filter sm:table-cell'
-                            >
+                                border-gray-300 bg-gray-50 bg-opacity-75 pl-2 py-2
+                                 text-left font-semibold text-gray-900 backdrop-blur
+                                  backdrop-filter sm:table-cell'
+                           >
                                 Name
                             </th> 
                             <th scope='col'
@@ -95,7 +95,7 @@ export default function AccountSupport() {
                         
                                 <tr key={index} className=' even:bg-gray-50 group'>
                                     {/* {setData(post)} */}
-                                    <td className='pl-4'>{post.name}</td>
+                                    <td className='whitespace-nowrap text-sm font-medium text-gray-900 pl-2 py-2'>{post.name}</td>
                                     <td className='pl-4'>{post.username}</td>
                                     <td className='pl-4'><button onClick={handleReset} className="text-blue-700 ml-5">{post.resetPassword} </button></td>
                                     <td className='pl-4'><button onClick={handleView} className="text-blue-700 ml-5">{post.viewHistory} </button></td>
