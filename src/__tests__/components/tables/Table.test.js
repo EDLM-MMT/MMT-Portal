31286d81af877704cc0e12ec49923f38cce93290
schittlur <schittlur@deloitte.com>
2023-02-17 15:03:36 -0500

    Table test

--- src/__tests__/components/tables/Table.test.js
+++ src/__tests__/components/tables/Table.test.js
@@ -55,4 +55,41 @@ describe('Table component', () => {
 
   });
 
+  it('calls the handleView function when the "Reset" button is clicked', () => {
+    const { getByText } = render(
+      <MemoryRouterProvider>
+        <Table rowsData={rowsData} columnTitles={columnTitles} />
+      </MemoryRouterProvider>
+
+    );
+    const viewButton = getByText('Reset');
+    fireEvent.click(viewButton);
+
+  });
+
+  it('calls the handleView function when the "Go to Career Counseling" button is clicked', () => {
+    const { getByText } = render(
+      <MemoryRouterProvider>
+        <Table rowsData={[["Go To Career Counseling"]]} columnTitles={columnTitles} />
+      </MemoryRouterProvider>
+
+    );
+    const viewButton = getByText('Go To Career Counseling');
+    fireEvent.click(viewButton);
+
+  });
+
+  it('calls the handleView function when the "Delete" button is clicked', () => {
+    const { getByText } = render(
+      <MemoryRouterProvider>
+        <Table rowsData={[["Delete"]]} columnTitles={columnTitles} />
+      </MemoryRouterProvider>
+
+    );
+    const viewButton = getByText('Delete');
+    fireEvent.click(viewButton);
+
+  });
+
+
 });
