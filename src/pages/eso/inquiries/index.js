f8c1715ac3aa15f8af66877737e14c8ee7e5c216
LFRMadrid <73301821+LFRMadrid@users.noreply.github.com>
2023-02-20 16:45:29 -0500

    Inquiries and View Inquiries page built

---
+++ src/pages/eso/inquiries/index.js
@@ -0,0 +1,48 @@
+import DefaultLayout from '@/components/layouts/DefaultLayout';
+import useStore from '@/store/store';
+import TwoChoiceCard from '@/components/cards/TwoChoiceCard';
+import AddBtn from '@/components/buttons/AddButton';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function ESOInquiryDashboard() {
+    
+    const [data, setData] = useState([]);
+    const [update, setUpdate] = useState(false);
+
+    useEffect(() => {
+        axios
+          .get('../api/inquiry')
+          .then((res) => {
+            console.log(res.data.inquiries);
+            setData(res.data.inquiries);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }, []);
+
+    console.log(data);
+
+    return (
+        <DefaultLayout>
+            <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
+                <h1 className='pb-4 border-b mt-4 mb-8 text-3xl font-semibold'>
+                    <div className='flex flex-row justify-between'>  
+                        All Inquiries
+                    </div> 
+                </h1>
+                <div className=' flex-col flex h-18 justify-center w-full gap-5'>
+                    {data?.map((card, index) => {
+                        return(
+                            <TwoChoiceCard key={index} title={card.title} description={card.description} 
+                            buttonLabel={card.status} data={data} card={card} degreeIndex={index} 
+                            firstRoutePath={card.firstRoutePath} viewRoutePath={card.secondRoutePath}
+                            toggleModalUpdate={setUpdate} type="ESO" />
+                        )
+                    })}
+                </div>
+            </div>
+        </DefaultLayout>
+    );
+}
