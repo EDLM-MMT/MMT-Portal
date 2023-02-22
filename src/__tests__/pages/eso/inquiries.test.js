5d67f10885f38ed36995708896f50f1476a092b5
schittlur <schittlur@deloitte.com>
2023-02-22 10:42:58 -0500

    inquires test case

--- src/__tests__/pages/eso/inquiries.test.js
+++ src/__tests__/pages/eso/inquiries.test.js
@@ -3,6 +3,36 @@ import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 import ESOInquiryDashboard from "@/pages/eso/inquiries";
 
 describe("Inquiries Page", () => {
+
+  const data = {
+    "inquiries": [
+      {
+          "id": 300,
+          "title": "Leadership Course",
+          "description": "Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet. Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet. ",
+          "status": "Close Inquiry",
+          "inquiry_status": "Open",
+          "firstRoutePath": "transcripts/basicTranscript",
+          "secondRoutePath": "inquiries/300",
+          "timestampCreated": "1/21/2023 1:43:20 PM",
+          "submitted_by": "Bill Phillips",
+          "inquiryComments": [
+              {
+                  "author": "John Doe",
+                  "title": "ESO",
+                  "comment": "Please provide more information to better assist you on our end",
+                  "timestamp": "1/24/2023 2:43:20 PM"
+              },
+              {
+                  "author": "Bill Phillips",
+                  "title": "",
+                  "comment": "Can I please get an ETA on when this can be resolved?",
+                  "timestamp": "1/24/2023 2:43:19 PM"
+              }
+          ]
+      }
+    ]
+  }
   it("should render the component", () => {
     const { getByText } = render(
         <MemoryRouterProvider>
@@ -10,7 +40,7 @@ describe("Inquiries Page", () => {
         </MemoryRouterProvider>
     );
 
-    // mockAxios.get.mockImplementation(() => Promise.resolve({ data: []}));
+    mockAxios.get.mockImplementation(() => Promise.resolve({ data: data}));
 
     expect(getByText('All Inquiries')).toBeInTheDocument();
 
