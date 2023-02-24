4c0295f93429378a68b51422a1d782ecffd8c491
LFRMadrid <73301821+LFRMadrid@users.noreply.github.com>
2023-02-24 15:54:39 -0500

    fix issue with viewInquiry page not displaying
    this was due to changes to the ViewCard component props but it has been addressed

--- src/pages/serviceMember/inquiries/[inquiryId].js
+++ src/pages/serviceMember/inquiries/[inquiryId].js
@@ -97,44 +97,42 @@ export default function InquiryView({inquiryId}) {
               My Inquiries </button> -{`>`} {inquiry.title}
             </div>
             <div className=' flex-col flex h-18 justify-center w-full gap-5'>
-                    <ViewCard key={inquiry.id} title={inquiry.title} description={inquiry.description}/>                   
-            </div>
-            <div className='bg-white w-full border h-50 mt-4 rounded-md border-gray-200 p-4 pb-0 shadow'>
-                Submitted on {inquiry.timestampCreated}
+                    <ViewCard key={inquiry.id} inquiry={inquiry}/>                   
             </div>
             <div className='bg-white w-full border h-50 mt-4 mb-4 rounded-md border-gray-200 p-4 pb-2 shadow'>
+              <div className="font-semibold text-xl mb-4 ">
+                Inquiry Timeline
+              </div>
               <form onSubmit={handlePost}>
                 <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add a comment:</label>
                 <input type="text-area" id="description" name="comment" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-5/6 p-2.5 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={"Please provide comments if necessary."} required />
                 <button className="flex justify-end items-center tect-sm gap-2 dod-500 rounded-md hover:shadow-md text-white bg-dod-500/80 hover:bg-blue-400 hover:text-white px-2 p-1.5 transform transition-all duration-150 ease-in-out border-dod-500 border-2 focus:ring-2 ring-dod-500 outline-none">Post</button>
               </form>
-            </div>
-            <div className="font-semibold text-xl">
-            Inquiry Timeline
-            </div>
-            {comments?.map((data, index) => {
-                  return(
-                      (data.title === 'ESO') ? (
-                        <div className='bg-white w-3/4 text-black border h-50 mt-2 mb-4 rounded-md border-gray-200 p-4 pb-2 shadow'>
-                          <div className="flex-row flex justify-between text-base mb-4 font-medium">
-                            <div className="flex-row flex pr-2 text-base mb-2 font-medium">{`${data.author} (${data.title})`}</div> 
-                            <div>{data.timestamp}</div>
-                          </div>
-                          <div className="text-sm pl-4">{data.comment}</div>
-                        </div>
-                      ):(
-                        <div className="mt-4 mb-4 ml-96">
-                          <div className='bg-dod-300 bg-opacity-50 w-3/4 text-black border h-50 mt-0 rounded-md border-gray-200 p-4 pb-2 shadow'>
+              {comments?.map((data, index) => {
+                    return(
+                        (data.title === 'ESO') ? (
+                          <div className='bg-white w-3/4 text-black border h-50 mt-2 mb-4 rounded-md border-gray-200 p-4 pb-2 shadow'>
                             <div className="flex-row flex justify-between text-base mb-4 font-medium">
-                              <div className="flex-row flex pr-2 text-base mb-2 font-medium">{data.author} </div> 
+                              <div className="flex-row flex pr-2 text-base mb-2 font-medium">{`${data.author} (${data.title})`}</div> 
                               <div>{data.timestamp}</div>
                             </div>
                             <div className="text-sm pl-4">{data.comment}</div>
                           </div>
-                        </div>
-                      )
-                  )
-              })}            
+                        ):(
+                          <div className="mt-4 mb-4 ml-96">
+                            <div className='bg-dod-300 bg-opacity-50 w-3/4 text-black border h-50 mt-0 rounded-md border-gray-200 p-4 pb-2 shadow'>
+                              <div className="flex-row flex justify-between text-base mb-4 font-medium">
+                                <div className="flex-row flex pr-2 text-base mb-2 font-medium">{data.author} </div> 
+                                <div>{data.timestamp}</div>
+                              </div>
+                              <div className="text-sm pl-4">{data.comment}</div>
+                            </div>
+                          </div>
+                        )
+                    )
+                })
+              }            
+            </div>
           </div>
         </DefaultLayout>
     );
