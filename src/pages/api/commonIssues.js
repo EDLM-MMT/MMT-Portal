e5baf5ffce03500bf98327ce9a3daf5a627e8fef
schittlur <schittlur@deloitte.com>
2023-03-13 18:00:22 -0400

    remove console logs

--- src/pages/api/commonIssues.js
+++ src/pages/api/commonIssues.js
@@ -1,6 +1,6 @@
 import commonIssues from "@/data/service_member/commonIssues.json"
 
 export default function handler(req, res) {
-    console.log("here", commonIssues);
+    // console.log("here", commonIssues);
     return res.status(200).json(commonIssues);
 }
