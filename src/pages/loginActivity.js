dad91f335326938bf2fb5b0d70efbb073d24dde5
Brian <brgriffiths@deloitte.com>
2023-03-14 09:31:47 -0500

    Login Activity page for each user

--- src/pages/loginActivity.js
+++ src/pages/loginActivity.js
@@ -1,22 +1,72 @@
 import DefaultLayout from '@/components/layouts/DefaultLayout';
 import Table from '@/components/tables/Table';
+import useStore from '@/store/store';
+
 
 export default function LoginActivity() {
+    const user = useStore((state) => state.userData);
 
-    const data = [
-        ["1/24/2023 2:43:20 PM", 1, 3, 1, 1, 2],
-        ["1/21/2023 3:43:20 PM", 3, 2, "", 2, 1],
-        ["1/20/2023 2:53:20 PM", 1, 3, 1, 1, 4],
-        ["1/20/2023 7:43:20 PM", 3, "", 2, 2, 1],
-        ["1/19/2023 9:45:20 PM", 1, 3, 1, "", 2],
-        ["1/18/2023 2:30:20 PM", "", 2, 1, 2, ""],
+    const dataServiceMember = [
+        ["1/24/2023 2:43:20 PM", 1, 3, 1, 1, 2, 2],
+        ["1/21/2023 3:43:20 PM", 3, 2, "", 2, 1, 1],
+        ["1/20/2023 2:53:20 PM", 1, 3, 1, 1, 4, ""],
+        ["1/20/2023 7:43:20 PM", 3, "", 2, 2, 1, ""],
+        ["1/19/2023 9:45:20 PM", 1, 3, 1, "", 2, 3],
+        ["1/18/2023 2:30:20 PM", "", 2, 1, 2, "", 2],
 
     ]
+    const dataProgramAdmin = [
+        ["1/24/2023 2:43:20 PM", 1, 3, 1],
+        ["1/21/2023 3:43:20 PM", 3, 2, ""],
+        ["1/20/2023 2:53:20 PM", 1, 3, 1],
+        ["1/20/2023 7:43:20 PM", 3, "", 2],
+        ["1/19/2023 9:45:20 PM", 1, 3, 1],
+        ["1/18/2023 2:30:20 PM", "", 2, 1],
+
+    ]
+    const dataESO = [
+        ["1/24/2023 2:43:20 PM", 1, 3, 1, 1],
+        ["1/21/2023 3:43:20 PM", 3, 2, "", 2],
+        ["1/20/2023 2:53:20 PM", 1, 3, 1, 1],
+        ["1/20/2023 7:43:20 PM", 3, "", 2, 2],
+        ["1/19/2023 9:45:20 PM", 1, 3, 1, ""],
+        ["1/18/2023 2:30:20 PM", "", 2, 1, 2],
+
+    ]
+
     return (
         <DefaultLayout >
         <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
             <h1 className='pb-4 border-b mb-8 text-3xl font-semibold'>Login Activity</h1>
-            <Table columnTitles={["Login Date/Time", "Trascripts", "Inquiries", "Degree Agreements", "Degree Pathways", "Quick Links"]} rowsData={data}/>
+
+            {/* {user?.role === 'Service Member' ((item) => {
+                return <Button key={item.label} data={item} />;
+            })}
+            {user?.role === 'Program Administrator' && ProgramAdminMenuItems.map((item) => {
+                return <Button key={item.label} data={item} />;
+            })}
+            {user?.role === 'ESO' && ESOMenuItems.map((item) => {
+                return <Button key={item.label} data={item} />;
+            })} */}
+
+
+
+            <div>
+                {user.role === "Service Member" &&
+                    <Table columnTitles={["Login Date/Time", "Trascripts", "Inquiries", "Degree Agreements", "Degree Pathways", "Career Counseling", "Quick Links"]} rowsData={dataServiceMember}/>            
+                }
+            </div>
+            <div>
+                {user.role === "Program Administrator" &&
+                    <Table columnTitles={["Login Date/Time", "Accounts Management", "ESO Management", "Quick Links"]} rowsData={dataProgramAdmin}/>            
+                }
+            </div>
+            <div>
+                {user.role === "ESO" &&
+                    <Table columnTitles={["Login Date/Time", "Inquiries", "Degree Pathways", "Career Counseling", "Quick Links"]} rowsData={dataESO}/>            
+                }
+            </div>
+            
 
         </div>
         </DefaultLayout>
