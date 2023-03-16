f48aa6aef1a8fa7002050638baed1da9c8d3cf20
schittlur <schittlur@deloitte.com>
2023-03-16 14:11:03 -0400

    unit tests

--- src/pages/serviceMember/degreePathways.js
+++ src/pages/serviceMember/degreePathways.js
@@ -298,7 +298,7 @@ export default function DegreePathways() {
                 <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
                     <h1 className='pb-4 border-b mt-4 mb-8 text-3xl font-semibold'>
                         <div className='flex flex-row justify-between'>  
-                            Degree Pathways Catalog sdf
+                            Degree Pathways Catalog
                         </div> 
                     </h1>
                     <div>
