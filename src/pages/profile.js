1cc45d58822c7012778e0e760c736dab17a11173
schittlur <schittlur@deloitte.com>
2023-04-03 12:54:52 -0400

    password and profile updates

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -11,14 +11,14 @@ export default function Profile() {
     return (
         <DefaultLayout >
         <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
-        <h1 className='my-2 pb-4 border-b mt-4 mb-8 text-3xl font-semibold'>
+        <h1 className='my-2 pb-4 border-b mt-4 text-3xl font-semibold'>
             <div className='flex flex-row justify-between'>  
                 Profile
-                <Button btnText={"Reset Password"} link={"/resetPassword"} className={"mt-0"}></Button>
+                <Button btnText={"Change Password"} link={"/resetPassword"} className={"mt-0"}></Button>
             </div> 
         </h1>
 
-        <div className='flex flex-row justify-between mt-20'>
+        <div className='flex flex-row justify-between'>
             <div></div>
             {userData?.role === 'Service Member' && 
             <div>
@@ -45,8 +45,19 @@ export default function Profile() {
                     <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Position/Role</label>
                     <input type="text" id="position" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={userData?.role} required />
                 </div>
+                <div>
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sector</label>
+                    <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={userData?.learner.personnel.organization.organizationname} required />
+                </div>  
+                <div>
+                    <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rank/Rating</label>
+                    <input type="text" id="position" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={userData?.learner.personnel.organization.organizationidentificationcode} required />
+                </div>
             </div>
-            <button type="submit" class="text-white bg-dod-500/80 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+            <div className='flex justify-between'>
+                <Button btnText={"Cancel"} link={"/dashboard"} className={"mt-0"}></Button>
+                <button type="submit" class="text-white bg-dod-500/80 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+            </div>
         </form>
 
         </div>
