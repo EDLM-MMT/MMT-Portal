de17fac62c07235f0f0ef89009858d4991a5a63a
schittlur <schittlur@deloitte.com>
2023-04-14 16:59:58 -0400

    credits page test

--- src/pages/serviceMember/credits/index.js
+++ src/pages/serviceMember/credits/index.js
@@ -5,93 +5,102 @@ import dynamic from 'next/dynamic';
 const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 import CreditsTable from '@/components/tables/CreditsTable'
 import PotentialCreditsTable from '@/components/tables/PotentialCreditsTable'
-
+import { useRouter } from "next/router"
 
 export default function Credits() {
 
-    const [credits, setCredits] = useState([])
+  const router = useRouter();
+  const [credits, setCredits] = useState([])
 
-    useEffect(() => {
-        axios
-          .get(`/api/credits`)
-          .then((res) => {
-            setCredits(res.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-    }, []);
+  useEffect(() => {
+      axios
+        .get(`/api/credits`)
+        .then((res) => {
+          setCredits(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }, []);
 
-    const state = {
-          
-        series: [12],
-        options: {
-          chart: {
-            type: 'radialBar',
-            offsetY: -20,
-            sparkline: {
-              enabled: true
-            }
-          },
-          plotOptions: {
-            radialBar: {
-              startAngle: -90,
-              endAngle: 90,
-              track: {
-                background: "#e7e7e7",
-                strokeWidth: '97%',
-                margin: 5, // margin is in pixels
-                dropShadow: {
-                  enabled: true,
-                  top: 2,
-                  left: 0,
-                  color: '#999',
-                  opacity: 1,
-                  blur: 2
-                }
-              },
-              dataLabels: {
-                name: {
-                  show: false
-                },
-                value: {
-                  offsetY: -2,
-                  fontSize: '22px'
-                }
-              }
-            }
-          },
-          grid: {
-            padding: {
-              top: -10
-            }
-          },
-          fill: {
-            type: 'solid',
-            // gradient: {
-            //   shade: 'light',
-            //   shadeIntensity: 0.4,
-            //   inverseColors: false,
-            //   opacityFrom: 1,
-            //   opacityTo: 1,
-            //   stops: [0, 50, 53, 91]
-            // },
-          },
-          labels: ['Average Results'],
-        },
-      
-    }
+  // const state = {
+        
+  //     series: [12],
+  //     options: {
+  //       chart: {
+  //         type: 'radialBar',
+  //         offsetY: -20,
+  //         sparkline: {
+  //           enabled: true
+  //         }
+  //       },
+  //       plotOptions: {
+  //         radialBar: {
+  //           startAngle: -90,
+  //           endAngle: 90,
+  //           track: {
+  //             background: "#e7e7e7",
+  //             strokeWidth: '97%',
+  //             margin: 5, // margin is in pixels
+  //             dropShadow: {
+  //               enabled: true,
+  //               top: 2,
+  //               left: 0,
+  //               color: '#999',
+  //               opacity: 1,
+  //               blur: 2
+  //             }
+  //           },
+  //           dataLabels: {
+  //             name: {
+  //               show: false
+  //             },
+  //             value: {
+  //               offsetY: -2,
+  //               fontSize: '22px'
+  //             }
+  //           }
+  //         }
+  //       },
+  //       grid: {
+  //         padding: {
+  //           top: -10
+  //         }
+  //       },
+  //       fill: {
+  //         type: 'solid',
+  //         // gradient: {
+  //         //   shade: 'light',
+  //         //   shadeIntensity: 0.4,
+  //         //   inverseColors: false,
+  //         //   opacityFrom: 1,
+  //         //   opacityTo: 1,
+  //         //   stops: [0, 50, 53, 91]
+  //         // },
+  //       },
+  //       labels: ['Average Results'],
+  //     },
+    
+  // }
+
+  const handleClick = () => {
+    router.push("/serviceMember/degreePathways");
+  }
 
     return (
         <DefaultLayout>
             <div className='bg-white w-full border rounded-md border-gray-200 p-4 shadow'>
-                <h1 className='pb-4 border-b mt-4 mb-8 text-3xl font-semibold'>
+                <h1 className='pb-4 border-b mt-4 text-3xl font-semibold'>
                     <div className='flex flex-row justify-between'>  
                         Credits Translation Page
                     </div> 
                 </h1>
-                
-                <div className="border rounded-md border-gray-200 p-4 shadow">
+                <div>
+                  <button onClick={handleClick}
+                  className='text-dod-500 hover:underline underline hover:text-blue-500 cursor-pointer mb-4 transition-all duration-150 ease-in-out'>                    
+                  Degree Pathways Catalog </button> -{`>`} BGS General Studies 
+                </div>
+                <div className="border rounded-md border-gray-200 p-4 shadow mt-4">
                     <h1 className='border-b mt-2 mb-6 text-xl font-semibold'>
                         <div className='flex flex-row justify-between'>  
                             BGS General Studies Overview
