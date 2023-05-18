0d535b569cf43b4b0d4659ff2358501e46e230a6
schittlur <schittlur@deloitte.com>
2023-05-18 09:39:11 -0400

    improving testing coverage

--- src/__tests__/components/common/Header.test.js
+++ src/__tests__/components/common/Header.test.js
@@ -4,24 +4,25 @@ import { act, fireEvent, render } from "@testing-library/react";
 import { MemoryRouterProvider } from 'next-router-mock/dist/MemoryRouterProvider';
 
 describe('Header', () => {
-    jest.mock('@/store/store', () =>
-    jest.fn(() => ({
-        userData: 
-        { 
-        role:'Service Member',
-        learner:
-            {personnel:
-            {person:
-                { name: 'Test user' } 
-            }
-            }
-        },
-        setUserData: jest.fn(),
-        removeUserData: jest.fn()
-        }))
-    );
 
     it('should show the header text', () => {
+        jest.mock('@/store/store', () =>
+        jest.fn(() => ({
+            userData: 
+            { 
+            role:'Service Member',
+            learner:
+                {personnel:
+                {person:
+                    { name: 'Test user' } 
+                }
+                }
+            },
+            setUserData: jest.fn(),
+            removeUserData: jest.fn()
+            }))
+        );
+
         const { getByText } = render( <MemoryRouterProvider>
             <Header />
             </MemoryRouterProvider>);
@@ -32,6 +33,36 @@ describe('Header', () => {
         act(() => {
             fireEvent.click(button);
         });
-      });
+    });
+
+    it('should show the header text', () => {
+        jest.mock('@/store/store', () =>
+        jest.fn(() => ({
+            userData: 
+            { 
+            role:'Program Administrator',
+            learner:
+                {personnel:
+                {person:
+                    { name: 'Test user' } 
+                }
+                }
+            },
+            setUserData: jest.fn(),
+            removeUserData: jest.fn()
+            }))
+        );
+        
+        const { getByText } = render( <MemoryRouterProvider>
+            <Header />
+            </MemoryRouterProvider>);
+
+        expect(getByText('Sign Up')).toBeInTheDocument();
+
+        const button = getByText('Sign Up');
+        act(() => {
+            fireEvent.click(button);
+        });
+    });
 
 });
