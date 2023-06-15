d2715f715c8912123077547eeb345b32ef5de5f3
schittlur <schittlur@deloitte.com>
2023-06-15 14:31:44 -0400

    login routing updated

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,20 +1,33 @@
-import serviceMemberData from '@/data/service_member/serviceMemberData.json';
+import serviceMemberData1 from '@/data/service_member/serviceMemberData1.json';
+import serviceMemberData2 from '@/data/service_member/serviceMemberData2.json';
+import serviceMemberData3 from '@/data/service_member/serviceMemberData3.json';
+
 import programAdminData from '@/data/programAdmin/programAdminData.json'
-import esoData from '@/data/eso/esoData.json'
+import esoData1 from '@/data/eso/esoData1.json'
+import esoData2 from '@/data/eso/esoData2.json'
 import execStakeholderData from '@/data/execStakeholder/execStakeholderData.json'
 
 export default function handler(req, res) {
   const { username, password } = req.body;
-  if (username.toLowerCase().includes('servicemember') && password.includes('password')) {
-    return res.status(200).json(serviceMemberData);
+  if (username.toLowerCase().includes('phillips@us.navy.mil') && password.includes('password')) {
+    return res.status(200).json(serviceMemberData1);
   }
-  if (username.toLowerCase().includes('programadmin') && password.includes('password')) {
+  if (username.toLowerCase().includes('william@us.navy.mil') && password.includes('password')) {
+    return res.status(200).json(serviceMemberData2);
+  }
+  if (username.toLowerCase().includes('smith@us.navy.mil') && password.includes('password')) {
+    return res.status(200).json(serviceMemberData3);
+  }
+  if (username.toLowerCase().includes('glass@us.navy.mil') && password.includes('password')) {
     return res.status(200).json(programAdminData);
   }
-  if (username.toLowerCase().includes('eso') && password.includes('password')) {
-    return res.status(200).json(esoData);
+  if (username.toLowerCase().includes('blanchard@us.navy.mil') && password.includes('password')) {
+    return res.status(200).json(esoData1);
   }
-  if (username.toLowerCase().includes('execstakeholder') && password.includes('password')) {
+  if (username.toLowerCase().includes('doe@us.navy.mil') && password.includes('password')) {
+    return res.status(200).json(esoData2);
+  }
+  if (username.toLowerCase().includes('martinez@us.navy.mil') && password.includes('password')) {
     return res.status(200).json(execStakeholderData);
   }
     return res.status(401).json()
